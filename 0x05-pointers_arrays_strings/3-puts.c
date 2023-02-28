1f595f1821a61917b7ccb8f8a45d7c37692c8405
Oluwatobi Gbadamosi <tobbybosch@gmail.com>
2023-02-28 05:02:14 -0800

    printing more strings

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - this will print strings
+ * @str: this is the function parameter
+ * Return: 0
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+
+}
