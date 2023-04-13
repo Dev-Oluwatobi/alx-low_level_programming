464aa14d67c3ec87a1f3b7ec70671aa41e30367f
Oluwatobi Gbadamosi <tobbybosch@gmail.com>
2023-04-13 07:25:59 -0700

    function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,37 +1,34 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <string.h>
+#include "main.h"
 
 /**
- * create_file - create array of chars
- * @text_content: NULL terminated string to be written to the file
- * @filenmae: file to be created
+ * create_file - this function creates an array of char.
  *
- * Return: 1 if succesful, -1 if unsucessful
+ * @text_content: NULL terminated string
+ * @filename: file name to be created
+ *
+ * Return: 1 if success, -1 on unsucessful
  */
 
-int create_file(const char *filename, char *text_content) {
-    if (filename == NULL) {
-        return -1;
-    }
-    
-    int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-    if (fd < 0) {
-        return -1;
-    }
-    
-    if (text_content != NULL) {
-        ssize_t len = strlen(text_content);
-        ssize_t bytes_written = write(fd, text_content, len);
-        if (bytes_written != len) {
-            close(fd);
-            return -1;
-        }
-    }
-    
-    close(fd);
-    return 1;
+int create_file(const char *filename, char *text_content)
+{
+	int o, w, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
+		return (-1);
+
+	close(o);
+
+	return (1);
 }
